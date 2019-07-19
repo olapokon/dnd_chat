e9d3fe42cce4f0b5a85d2d0dffc8a28524de37c0
Mibburo <c.bitsikas@gmail.com>
2019-07-19 11:09:54 +0300

    use bootstrap button for drop down

--- client/src/components/CharacterSheet/CharacterSheetComponents/LoadMenu.js
+++ client/src/components/CharacterSheet/CharacterSheetComponents/LoadMenu.js
@@ -3,7 +3,7 @@ import React from 'react';
 function LoadMenu(props) {
   return (
     <div id="loadMenu">
-      <select name="loadMenu" onChange={props.handleLoad}>
+      <select name="loadMenu" class="charSelect btn btn-light" onChange={props.handleLoad}>
         <option>Choose Character</option>
         <option value="new">Create New Character</option>
         {props.user &&
